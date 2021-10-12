834aed07702026dc3342818ea45f0ad786c6d860
Vaida Maslinskiene <vaidutee@gmail.com>
2021-10-12 11:51:30 +0300

    mano darbas

---
+++ main.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+    document.querySelector("#menu").classList.toggle("change");
+    document.querySelector(".nav-ul").classList.toggle("change");
+    document.querySelector("#menu-bg").classList.toggle("change-bg");
+}
